926a04328d4cf0666a548835e70f2bc3fe927857
Felix Honer <fhoner@stud.hs-heilbronn.de>
2017-04-13 16:45:01 +0200

    cleaned up sample config

--- config_sample.php
+++ config_sample.php
@@ -1,31 +1,35 @@
 <?php
 
-DEFINE("URL", "http://localhost/planer");
+/* HOST SETUP */
+DEFINE("URL", "http://localhost/planer");                   // full url
 DEFINE("BASEDIR", $_SERVER['DOCUMENT_ROOT'] . "/planer/");	// base dir of cms including document root
 DEFINE("ROOT", "/planer/");									// base dir of cms without document root
-DEFINE("TEMPLATEDIR", BASEDIR . "template/");				// directory of template files
-DEFINE("PLUGINDIR", BASEDIR . "plugins/");					// directory of plugins
-DEFINE("LANGUAGE", "de-DE");								// used language file
-DEFINE("FRONTENDURL", "http://localhost/planer/");
-DEFINE("ORGANISATION", "Organisation Ltd.");
-DEFINE("COMPRESS_ENABLED", false);
-DEFINE("ENFORCE_SSL", false);								// redirect to https
-DEFINE("WEBMASTER", "webmaster@domain.com");
+DEFINE("ORGANISATION", "Organisazion Ltd.");                // organisation name
+DEFINE("WEBMASTER", "info@domain.de");                      // email of webmaster
 
+/* REQUIRED */
 require("dbConn.php");
 require("template.php");
+require_once("emailSettings.php");
+require_once(BASEDIR . "vendor/autoload.php");
 
+/* DATABASE SETUP */
 dbConn::setHost("localhost");
 dbConn::setDatabase("planer");
 dbConn::setUsername("root");
 dbConn::setPassword("root");
 dbConn::setTablePrefix("planer_");
 
-require_once("emailSettings.php");
+/* EMAIL SETUP */
 emailSettings::setHost("localhost");
 emailSettings::setUsername("user@domain.com");
 emailSettings::setPassword("53cur3");
 emailSettings::setSender(WEBMASTER);
 emailSettings::setMethod(emailSendMethod::PHPMAILER);
+
+/* DO NOT CHANGE */
+DEFINE("TEMPLATEDIR", BASEDIR . "template/");
+DEFINE("PLUGINDIR", BASEDIR . "plugins/");
+DEFINE("FRONTENDURL", URL);
 
 ?>
