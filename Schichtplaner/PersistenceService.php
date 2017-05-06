7b4a636654f27dc8776cf360cb1ba9c899d10a18
Felix Honer <fhoner@stud.hs-heilbronn.de>
2017-05-06 17:14:06 +0200

    fixed autocomplete on custom table prefix

--- Schichtplaner/PersistenceService.php
+++ Schichtplaner/PersistenceService.php
@@ -16,8 +16,8 @@ class PersistenceService {
      */
     public static function getAllWorkers() {
         $arr = [];
-        foreach (dbConn::query("SELECT DISTINCT w.name, w.email, w.plan  FROM `schichtplaner_worker` as w
-                                INNER JOIN schichtplaner_plan AS p ON w.plan = p.name
+        foreach (dbConn::query("SELECT DISTINCT w.name, w.email, w.plan  FROM :prefix:worker as w
+                                INNER JOIN :prefix:plan AS p ON w.plan = p.name
                                 WHERE p.deleted = 0
                                 ORDER BY name, email ASC") as $r) {
             $arr[] = $r;
